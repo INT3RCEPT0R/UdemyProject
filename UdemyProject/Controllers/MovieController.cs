4869f90923976a90bb6c5850f06b0cf3f56bdc64
Janek <jan.k.marcinkowski@gmail.com>
2020-09-25 16:28:03 +0200

    ActionResult edition

--- UdemyProject/Controllers/MovieController.cs
+++ UdemyProject/Controllers/MovieController.cs
@@ -13,7 +13,11 @@ namespace UdemyProject.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            //return View(movie);
+            //return Content("Hello world!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index","Home", new { page = 1, sortBy = "name"});
         }
     }
 }
