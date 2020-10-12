ae53eb8d5f4608e6f2467d2c6cba270260f22f64
Janek <jan.k.marcinkowski@gmail.com>
2020-10-12 18:14:01 +0200

    Some action parameters and routing added

--- UdemyProject/App_Start/RouteConfig.cs
+++ UdemyProject/App_Start/RouteConfig.cs
@@ -1,4 +1,4 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +12,13 @@ namespace UdemyProject
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "MovieByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action = "ByReleaseDate" },
+                new { year = @"\d{4}", month=@"\d{2}"}
+                );
 
             routes.MapRoute(
                 name: "Default",
---
+++ UdemyProject/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UdemyProject.Models;
+
+namespace UdemyProject.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
+            //return Content("Hello world!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index","Home", new { page = 1, sortBy = "name"});
+        }
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+        public ActionResult Edit(int movieId)
+        {
+            return Content("id=" + movieId);
+        }
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+    }
+}
